81a54d23f5265dc1d783c61f0bd9a7b1fd5b844b
eliotgcruzp <egcruz@guanajuato.gob.mx>
2023-09-29 13:02:19 -0600

    Borrado

--- Shopall/src/main/java/com/example/shopall/repository/PerfilRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopall.repository;
-
-import com.example.shopall.data.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
-}
